c8431a6532ea11d9ac3dfffb610b2029df2d1d8b
Bart Dority <b0rgBart3@gmail.com>
2017-12-30 21:47:25 -0800

    adjusting for errors

--- src/app/materials/material.component.ts
+++ src/app/materials/material.component.ts
@@ -1,48 +1,48 @@
-import { Component, OnInit, Output, Input, OnChanges } from '@angular/core';
-import { Router } from '@angular/router';
-import { Course } from '../models/course.model';
-import { MaterialService } from '../services/material.service';
-import { Material } from '../models/material.model';
-import { Section } from '../models/section.model';
-import { Globals } from '../globals';
+// import { Component, OnInit, Output, Input, OnChanges } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { Course } from '../models/course.model';
+// import { MaterialService } from '../services/material.service';
+// import { Material } from '../models/material.model';
+// import { Section } from '../models/section.model';
+// import { Globals } from '../globals';
 
-@Component({
-    moduleId: module.id,
-    selector: 'material',
-    templateUrl: 'material.component.html',
-    styleUrls: ['material.component.css']
-})
+// @Component({
+//     moduleId: module.id,
+//     selector: 'material',
+//     templateUrl: 'material.component.html',
+//     styleUrls: ['material.component.css']
+// })
 
-export class MaterialComponent implements OnInit {
+// export class MaterialComponent implements OnInit {
 
-    public icon: string;
-    public reference: string;
-    public iconref: string;
+//     public icon: string;
+//     public reference: string;
+//     public iconref: string;
 
-    @Input() material: Material;
-    constructor(private globals: Globals) {
+//     @Input() material: Material;
+//     constructor(private globals: Globals) {
 
-    }
+//     }
 
-    ngOnInit() {
-        if (this.material.contenturl) {
-            this.reference = this.material.contenturl;
-         } else {
-             this.reference = this.globals.materialfiles + this.material.id + '/' + this.material.file;
-         }
+//     ngOnInit() {
+//         if (this.material.contenturl) {
+//             this.reference = this.material.contenturl;
+//          } else {
+//              this.reference = this.globals.materialfiles + this.material.id + '/' + this.material.file;
+//          }
 
-        if (this.material.type) {
-        switch (this.material.type) {
-            case 'PDF document':
-              this.icon = 'pdf';
-              break;
-            case 'video':
-              this.icon = 'video';
-              break;
-            default:
-              break;
-        }
-        this.iconref = '../assets/images/' + this.icon + '.png';
-    }}
+//         if (this.material.type) {
+//         switch (this.material.type) {
+//             case 'PDF document':
+//               this.icon = 'pdf';
+//               break;
+//             case 'video':
+//               this.icon = 'video';
+//               break;
+//             default:
+//               break;
+//         }
+//         this.iconref = '../assets/images/' + this.icon + '.png';
+//     }}
 
-}
+// }
