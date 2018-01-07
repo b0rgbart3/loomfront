fb8957262a9a013f21117b966ffde35db2c16ca9
Bart Dority <b0rgBart3@gmail.com>
2018-01-06 17:51:47 -0800

    upload materialfiles

--- src/app/materials/modal/modal.component.ts
+++ src/app/materials/modal/modal.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MaterialCollection } from '../../models/materialcollection.model';
 import { Globals } from '../../globals';
-
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     moduleId: module.id,
@@ -14,12 +14,13 @@ import { Globals } from '../../globals';
 export class ModalComponent implements OnInit {
     @Input() modalURL: string;
     @Output() closeMe = new EventEmitter<boolean>();
+    safeURL: any;
 
-
-    constructor( private globals: Globals) {    }
+    constructor( private globals: Globals, private domSanitizer: DomSanitizer) {    }
 
     ngOnInit() {
-        this.modalURL = this.modalURL + ' | safe' + '&embbeded=true';
+        this.safeURL = this.domSanitizer.bypassSecurityTrustResourceUrl(this.modalURL);
+        this.modalURL = 'http://docs.google.com/gview?url=' + this.safeURL + '&embbeded=true';
 
     }
 
