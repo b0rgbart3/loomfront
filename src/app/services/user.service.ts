1ef432f69bd70312258b5de43e4a60a541712e7e
Bart Dority <b0rgBart3@gmail.com>
2018-01-16 19:11:09 -0800

    adjusting navbar avatar

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -146,10 +146,11 @@ export class UserService implements OnInit {
             // on the API server
             if (!this.users[i].facebookRegistration) {
               this.users[i].avatar_URL = this._avatar_image_url + this.users[i].id + '/' + this.users[i].avatar_filename;
-            }
+
             if ( (this.users[i].avatar_filename === undefined) || (this.users[i].avatar_filename === '')) {
               // console.log('setting placeholder');
               this.users[i].avatar_URL = this.globals.avatars + 'placeholder.png';
+            }
             }
           }
           // console.log('All: ' + JSON.stringify(data));
