aacfb02a5b1eaeb43a4156521ff44ed943a23c12
Bart Dority <b0rgBart3@gmail.com>
2018-01-27 06:39:58 -0800

    adjusting express commands in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 //app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/dist');
 });
 
 // Start the app by listening on the default Heroku port
