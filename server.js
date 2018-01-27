c681c1bf16ca8d49a7427b0da89e28d38c9f792d
Bart Dority <b0rgBart3@gmail.com>
2018-01-27 07:23:47 -0800

    using angularcasts routing solution

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //Install express server
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res){
-    res.sendFile(__dirname + '/dist');
+    res.sendFile(path.join(__dirname + '/dist/index.html') );
   });
   
 // Start the app by listening on the default Heroku port
