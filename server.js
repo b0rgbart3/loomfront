5eda84d48833ee1d4a1e15a797eab8ec0290cdb7
Bart Dority <b0rgBart3@gmail.com>
2018-01-27 06:45:22 -0800

    adjusting express commands in server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('*', function(req, res){
-  res.sendFile(__dirname + '/dist');
-});
-
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/dist');
+  });
+  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
 
 
