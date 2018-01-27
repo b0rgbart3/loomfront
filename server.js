ec045d561829cb65ead2b9b5cdeb303f6c499a0d
Bart Dority <b0rgBart3@gmail.com>
2018-01-27 07:04:10 -0800

    force ssl and use pathlocationstrategy

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 //Install express server
+// const express = require('express');
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist'));
+
+// app.get('/*', function(req, res){
+//     res.sendFile(__dirname + '/dist');
+//   });
+  
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
+//-------------
+
+// server.js
 const express = require('express');
 const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res){
-    res.sendFile(__dirname + '/dist');
-  });
-  
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
