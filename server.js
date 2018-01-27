6989c6f8525599e41844ca0f29b8dbe4af34c457
Bart Dority <b0rgBart3@gmail.com>
2018-01-27 09:37:48 -0800

    forcing https

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const path = require('path');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.get("*", function (req, res, next) {
+  res.redirect("https://" + req.headers.host + "/" + req.path);
+});
 
 app.get('/*', function(req, res){
+
     res.sendFile(path.join(__dirname + '/dist/index.html') );
+  
+  
   });
   
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
 
