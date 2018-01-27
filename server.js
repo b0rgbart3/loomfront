ab42fc666cdb05f308327356966de4ac7956a6cb
Bart Dority <b0rgBart3@gmail.com>
2018-01-26 20:25:02 -0800

    mucking with routing

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/dist');
+  });
+  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
